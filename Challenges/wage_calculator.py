1b1d27565f5b8b8ed78898e2ca314a53943a2a98
m11507 <123614087+m11507@users.noreply.github.com>
2024-06-13 09:23:50 -0500

    Update wage_calculator.py

--- Challenges/wage_calculator.py
+++ Challenges/wage_calculator.py
@@ -14,16 +14,23 @@ def get_nonnegative_float(text, MUST_BE_LESS_THAN_24):
             print("Error: Please enter a nonnegative numerical value.")
     return user_input
 
+def main():
+    while(True):
+        tax_rate = 0.12
+        days_worked = 350
+        hours_worked_daily = get_nonnegative_float("Enter the number of hours worked daily: ", True)
+        hourly_wage = get_nonnegative_float("Enter the hourly wage: ", False)
 
+        total_income = hours_worked_daily * hourly_wage * days_worked * (1-tax_rate)
 
-hours_worked_daily = get_nonnegative_float("Enter the number of hours worked daily: ", True)
-hourly_wage = get_nonnegative_float("Enter the hourly wage: ", False)
-
-total_income = hours_worked_daily * hourly_wage * 350 * 0.88
-
-print("\nPay Advice\n-------------")
-print(f"Hours Worked: {hours_worked_daily}")
-print(f"Hourly Wage: ${hourly_wage}")
-print(f"Wages Before Taxes: ${hours_worked_daily * hourly_wage * 350:.2f}")
-print(f"Tax Amount: ${hours_worked_daily * hourly_wage * 350 * 0.12:.2f}")
-print(f"Annual Wage After Taxes: ${total_income:.2f}")
+        print("\nPay Advice\n-------------")
+        print(f"Hours Worked: {hours_worked_daily}")
+        print(f"Hourly Wage: ${hourly_wage}")
+        print(f"Wages Before Taxes: ${hours_worked_daily * hourly_wage * days_worked:.2f}")
+        print(f"Tax Amount: ${hours_worked_daily * hourly_wage * days_worked * tax_rate:.2f}")
+        print(f"Annual Wage After Taxes: ${total_income:.2f}")
+        yesno = input("\nWould you like to run this program again? (y/n)")
+        if(yesno == "n" or yesno =="no"):
+            break
+    print("Goodbye!")
+main()
